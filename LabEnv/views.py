ecd70c2e7d72a029627d9c75c1c8fbe10cedb4e9
Didizy <am_nikishin@mail.ru>
2022-01-30 23:35:52 +1000

    Commit development

--- LabEnv/views.py
+++ LabEnv/views.py
@@ -1,10 +1,6 @@
 from rest_framework.views import APIView
-from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
-from rest_framework import status
-from .enviroment import Enviroment
 from .status import *
-import os
 
 
 class EnvView(APIView):
