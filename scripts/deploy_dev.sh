8ef2de76a7315add80d3063be45eea54c3d609d0
Didizy <am_nikishin@mail.ru>
2022-01-30 22:01:36 +1000

    Commit development

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -5,4 +5,4 @@ git add .
 git commit -m "Commit development"
 git push origin main
 
-ssh -i C:/Users/bulld/.ssh/id_rsa root@159.65.112.23 'cd ~/development/LabEnv/scripts && sh build.sh'
+ssh -i C:/Users/bulld/.ssh/id_rsa root@159.65.112.23 'cd .. && cd home/development/LabEnv/scripts && sh build.sh'
